72c00ba9c07cdbfef7f3587255cb9efcdd902167
joshfermin <joshfermin@gmail.com>
2015-04-07 18:04:52 -0600

    changed tweets_processed to sent_test and added a little bit of documentation:

--- mapreduce/flatten.js
+++ mapreduce/flatten.js
@@ -2,6 +2,7 @@ var mongoose    = require('mongoose'),
     db          = require('../tweet_collector/flattenmongo.js'),
     Tweet 		= db.tweetInit();
 
+// takes out attrs from value and flattens them. outputs to db named flattened.
 Tweet.aggregate([
 		{ $project: { 'latitude': '$value.latitude', 'longitude': '$value.longitude', 'text': '$value.text','sentiment': '$value.sentiment', 'created_at': '$value.created_at'}},
 		{ $out : "flattened" }
@@ -11,7 +12,7 @@ Tweet.aggregate([
             return;
         }
         console.log("db flattened");
-        
-    });
+
+});
 
 
