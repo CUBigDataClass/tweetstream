e8ca47c85d6799bec30ea90758c71a3a9ab41a5a
joshfermin <joshfermin@gmail.com>
2015-04-17 15:51:54 -0600

    getting average sentiment

--- frontend/client/src/controllers/home.js
+++ frontend/client/src/controllers/home.js
@@ -11,44 +11,43 @@ angular.module('app').controller('HomeController', [
         highlightOnHover: true,
       },
      done: function(datamap) {
-          datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-              // alert(geography.properties.name);
-              var state = geography.properties.name
-              if(state == "Pennsylvania") state = "penna"
-               $http({
-                  method: 'GET',
-                  url: '/tweets/'+state
-                 }).then(function(tweets){
-                  $scope.sent = tweets.data;
-                 });
-          });
+        datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+        // alert(geography.properties.name);
+        var state = geography.properties.name
+        if(state == "Pennsylvania") state = "penna"
+         $http({
+            method: 'GET',
+            url: '/tweets/'+state
+           }).then(function(tweets){
+            $scope.sent = tweets.data;
+            var totalSent = 0;
+            for(var index in tweets.data){
+              tweetSent = tweets.data[index].sentiment
+              totalSent += tweetSent
+            }
+
+            averageSent = totalSent/(tweets.data.length)
+            // console.log(totalSent)
+            // console.log(tweets.data.length)
+            console.log("average" + averageSent)
+
+
+           });
+        });
       }
    });
-    // var map = new Datamap({
-    //     element: document.getElementById('container'),
-    //     done: function(datamap) {
-    //         datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-    //             alert(geography.properties.name);
-    //         });
-    //     }
-    // });
 
   $scope.getBubbles = function(map,sent){
         map.bubbles(sent, {
         popupTemplate: function (geo, data) { 
-                return ['<div class="hoverinfo">' +  data.text,
-                '<br/>created_at: ' +  data.created_at,
-                '<br/>Sentiment: ' +  data.sentiment + '',
-                '</div>'].join('');
+          return ['<div class="hoverinfo">' +  data.text,
+          '<br/>created_at: ' +  data.created_at,
+          '<br/>Sentiment: ' +  data.sentiment + '',
+          '</div>'].join('');
         }
       });
     }
-
-
-
-}
-
-
+  }
 ]);
 
 
