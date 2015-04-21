0ee37c87319016e09827d9d4774ed76796ad08c5
joshfermin <joshfermin@gmail.com>
2015-04-21 12:37:35 -0600

    changing over to jquery datepicker, a lot less janky

--- frontend/client/src/controllers/home.js
+++ frontend/client/src/controllers/home.js
@@ -4,7 +4,24 @@ angular.module('app').controller('HomeController', [
 
   function($scope,$http){
 
-
+  $(function() {
+    $( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
+  });
 
   $scope.map = new Datamap({
 
